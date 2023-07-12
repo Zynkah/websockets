81024ec4a3433ea4b5c213061cae4973918d9bc7
Zynkah <ffgurl07@yahoo.com>
2023-07-12 12:26:55 -0500

    push

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,74 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { Text, Grid, Card } from "@nextui-org/react";
+import styles from "./page.module.css";
+
+const ws = new WebSocket("ws://beeps.gg/stream");
+// const message_user = document.getElementById("message_user");
+ws.addEventListener("open", () => {
+  console.log("connected");
+});
+ws.addEventListener("message", (event) => {
+  console.log(event);
+  // document.getElementById("message").innerText = event.data;
+});
+
+ws.onmessage = (event) => {
+  const message_user = JSON.parse(event.data);
+  console.log(message_user.user.username);
+  document.getElementById("message_user").innerText = message_user.user.username;
+  // message_user.innerText = message_user.user.username;
+};
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Grid.Container gap={2} justify="center">
+        <Grid xs={4}>
+          <Card isHoverable css={{ mw: "450px", backgroundColor: "#bd8c7d" }}>
+            <Card.Header>
+              <Text
+                h1
+                size={60}
+                css={{
+                  color: "#ffffff",
+                  padding: "1rem",
+                }}
+                weight="bold"
+              >
+                Events Rate Per Minute
+              </Text>
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+              <Text
+                p
+                size={12}
+                css={{
+                  color: "#ffffff",
+                  padding: "1rem",
+                }}
+                weight="bold"
+              >
+                Total Events
+              </Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={8}>
+          <Card isHoverable css={{ mw: "600px" }}>
+            <Text
+              h1
+              size={14}
+              css={{
+                color: "#49494b",
+                padding: "1rem",
+              }}
+              weight="bold"
+              id="message_user"
+            ></Text>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </main>
-  )
+  );
 }
